5e0bb68215a9d8672f2985206c2bba0d652b3f71
Roshan Verma <165799527+rkverma77@users.noreply.github.com>
2026-06-13 12:23:56 +0530

    Add files via upload

--- Frontend/src/features/interview/hooks/useInterview.js
+++ Frontend/src/features/interview/hooks/useInterview.js
@@ -13,7 +13,7 @@ export const useInterview = () => {
         throw new Error("useInterview must be used within an InterviewProvider")
     }
 
-    const { loading, setLoading, report, setReport, reports, setReports } = context
+    const { loading, setLoading, pdfLoading, setPdfLoading, report, setReport, reports, setReports } = context
 
     const generateReport = async ({ jobDescription, selfDescription, resumeFile }) => {
         setLoading(true)
@@ -60,7 +60,7 @@ export const useInterview = () => {
     }
 
     const getResumePdf = async (interviewReportId) => {
-        setLoading(true)
+        setPdfLoading(true)
         let response = null
         try {
             response = await generateResumePdf({ interviewReportId })
@@ -74,7 +74,7 @@ export const useInterview = () => {
         catch (error) {
             console.log(error)
         } finally {
-            setLoading(false)
+            setPdfLoading(false)
         }
     }
 
@@ -86,6 +86,6 @@ export const useInterview = () => {
         }
     }, [ interviewId ])
 
-    return { loading, report, reports, generateReport, getReportById, getReports, getResumePdf }
+    return { loading, pdfLoading, report, reports, generateReport, getReportById, getReports, getResumePdf }
 
 }
